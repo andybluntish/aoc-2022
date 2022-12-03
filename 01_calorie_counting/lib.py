eea2003631539f82e6b166330fef6876edd7e15f
Andy Stanford-Bluntish <andy@bluntish.net>
2022-12-03 13:38:40 +1030

    cleanup

--- 01_calorie_counting/lib.py
+++ 01_calorie_counting/lib.py
@@ -1,7 +1,6 @@
 def calories_per_elf(input_path):
     with open(input_path, 'r') as file:
-        data = file.read()
-        elves = data.strip().split("\n\n")
+        elves = file.read().strip().split("\n\n")
         results = []
 
         for elf in elves:
@@ -9,10 +8,6 @@ def calories_per_elf(input_path):
             results.append(sum(fruit))
 
         return results
-
-def calories_per_elf_golf(input_path):
-    with open(input_path, 'r') as file:
-        return map(lambda elf: sum(map(int, elf.split("\n"))), file.read().strip().split("\n\n"))
 
 def ex1(input_path):
     return max(calories_per_elf(input_path))
