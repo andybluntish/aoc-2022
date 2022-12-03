bcd24b161801ab4b416065dbff544de98a81af15
Andy Stanford-Bluntish <andy@bluntish.net>
2022-12-03 11:18:03 +1030

    add tests for with the real input
    allows safe refactoring of the solution

--- 01_calorie_counting/test.py
+++ 01_calorie_counting/test.py
@@ -1,14 +1,22 @@
 import unittest
 from lib import ex1, ex2
 
-input = './test_input.txt'
+test_input = './input_test.txt'
+real_input = './input.txt'
 
 class Tests(unittest.TestCase):
-    def test_ex1(self):
-        self.assertEqual(ex1(input), 24000)
+    # Part 1
+    def test_ex1_test_input(self):
+        self.assertEqual(ex1(test_input), 24000)
 
-    def test_ex2(self):
-        self.assertEqual(ex2(input), 45000)
+    def test_ex1_real_input(self):
+        self.assertEqual(ex1(real_input), 69795)
 
+    # Part 2
+    def test_ex2_test_input(self):
+        self.assertEqual(ex2(test_input), 45000)
+
+    def test_ex2_real_input(self):
+        self.assertEqual(ex2(real_input), 208437)
 if __name__ == '__main__':
     unittest.main()
--- 02_rock_paper_scissors/test.py
+++ 02_rock_paper_scissors/test.py
@@ -1,14 +1,22 @@
 import unittest
 from lib import ex1, ex2
 
-input = './test_input.txt'
+test_input = './input_test.txt'
+real_input = './input.txt'
 
 class Tests(unittest.TestCase):
-    def test_ex1(self):
-        self.assertEqual(ex1(input), 15)
+    # Part 1
+    def test_ex1_test_input(self):
+        self.assertEqual(ex1(test_input), 15)
 
-    def test_ex2(self):
-        self.assertEqual(ex2(input), 12)
+    def test_ex1_real_input(self):
+        self.assertEqual(ex1(real_input), 10718)
 
+    # Part 2
+    def test_ex2_test_input(self):
+        self.assertEqual(ex2(test_input), 12)
+
+    def test_ex2_real_input(self):
+        self.assertEqual(ex2(real_input), 14652)
 if __name__ == '__main__':
     unittest.main()
